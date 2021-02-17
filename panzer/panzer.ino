94ffcd5a4523da56d85ff64eb3afdc9d1bb652fd
muhammad Dallah <muhammaddallah093@gmail.com>
2021-02-17 13:05:10 +0100

    Add basic ServoMowement

--- panzer/panzer.ino
+++ panzer/panzer.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <analogWrite.h>
+#include <Servo.h>
 
 #define a1 26
 #define a2 25
@@ -11,6 +12,8 @@
 #define bSpeed 5
 #define Leds 17
 #define Fire 16
+#define servoRLpin 12
+#define servoUDpin 13
 
 char auth[] = "qbOwS1e6ZyWH49HYNchxHtK1CkfiiXJD";
 char ssid[] = "DRAGON";
@@ -19,16 +22,53 @@ char pass[] = "P48palaestina";
 int minRange = 312;
 int maxRange = 712;
 
-int minSpeed = 100;
-int maxSpeed = 255;
+int minSpeed = 150;
+int maxSpeed = 200; //maximal. 255
 int noSpeed = 0;
 
+int posRL = 60;
+int posUD = 60;
+Servo servoRL;
+Servo servoUD;
 
 BLYNK_WRITE(V1)
 {
   int x = param[0].asInt();
   int y = param[1].asInt();
   moveControl(x, y);
+}
+
+BLYNK_WRITE(V2)
+{
+  posUD--;
+  if (posUD < 35) {
+    posUD = 35;
+  }
+  servoUD.write(posUD);
+}
+BLYNK_WRITE(V8)
+{
+  posUD++;
+  if (posUD > 80) {
+    posUD = 80;
+  }
+  servoUD.write(posUD);
+}
+BLYNK_WRITE(V4)
+{
+  posRL++;
+  if (posRL > 120) {
+    posRL = 120;
+  }
+  servoRL.write(posRL);
+}
+BLYNK_WRITE(V6)
+{
+  posRL--;
+  if (posRL < 0) {
+    posRL = 0;
+  }
+  servoRL.write(posRL);
 }
 
 void moveControl(int x, int y)
@@ -91,6 +131,12 @@ void setup()
   pinMode(b1, OUTPUT);
   pinMode(b2, OUTPUT);
   pinMode(bSpeed, OUTPUT);
+
+  servoRL.attach(servoRLpin);
+  servoUD.attach(servoUDpin);
+
+  servoRL.write(60);
+  servoUD.write(60);
 
   // Debug console
   Serial.begin(9600);
